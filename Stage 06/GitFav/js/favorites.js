e9bef7e7633791a4c1cc2216b14f9bb0f4565a00
guiogigo <guilhermevbatista20@gmail.com>
2023-05-30 17:57:57 -0300

    Added GitFav

---
+++ Stage 06/GitFav/js/favorites.js
@@ -0,0 +1,151 @@
+import { GitHubUser } from './gitHubUser.js'
+
+export class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root)
+    this.load()
+    this.tbody = this.root.querySelector('table tbody')
+  }
+
+  save() {
+    localStorage.setItem('@github-gitFavs:', JSON.stringify(this.entries))
+  }
+
+  async add(username) {
+    try {
+
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      if(userExists) {
+        throw new Error('Usuário já cadastrado')
+      }
+
+      const user = await GitHubUser.search(username)
+
+      if(user.login === undefined) {
+        throw new Error('Usuário não encontrado')
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+      
+    } catch(error) {
+      alert(error.message)
+    }
+  }
+
+  load() {
+    this.entries = JSON.parse(localStorage.getItem('@github-gitFavs:')) || []
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries
+      .filter(entry => entry.login !== user.login)
+
+    this.entries = filteredEntries
+    this.update()
+    this.save()
+  }
+
+}
+
+export class FavoritesView extends Favorites {
+  constructor(root) {
+    super(root)
+
+    this.update()
+    this.onadd()
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+
+      this.add(value)
+    }
+  }
+
+  update() {
+    this.removeAllTr()
+
+    this.entries.forEach(user => {
+      const row = this.createRow()
+
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.public_repos').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+        if(isOk) {
+          this.delete(user)
+        }
+      }
+
+      this.tbody.append(row)
+    })
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = `
+      <td class="user">
+        <img src="https://github.com/guiogigo.png" alt="">
+        <a href="https://github.com/guiogigo" target="_blank">
+          <p>Guilherme Batista</p>
+          <span>guiogigo</span>
+        </a>
+      </td>
+      <td class="public_repos">
+        16
+      </td>
+      <td class="followers">
+        17  
+      </td>
+      <td>
+        <button class="remove">Remover</button>
+      </td>
+    `
+
+    document.querySelector('#noFav').classList.add('hide')
+    return tr
+  }
+
+  noFavorites() {
+    tr.innerHTML = `
+      <td class="user">
+        <img src="https://github.com/guiogigo.png" alt="">
+        <a href="https://github.com/guiogigo" target="_blank">
+          <p>Guilherme Batista</p>
+          <span>guiogigo</span>
+        </a>
+      </td>
+      <td class="public_repos">
+        16
+      </td>
+      <td class="followers">
+        17  
+      </td>
+      <td>
+        <button class="remove">Remover</button>
+      </td>
+    `
+
+    return tr
+  }
+
+  removeAllTr() {
+
+    this.tbody.querySelectorAll('tr')
+      .forEach((tr) => {
+        tr.remove()
+     })
+  }
+}
