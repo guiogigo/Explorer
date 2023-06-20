414245e6feac32ed09876099834c355d15f3f9a1
guiogigo <guilhermevbatista20@gmail.com>
2023-06-20 19:13:05 -0300

    Stage 08 Validations

--- Stage 08/api/src/routes/users.routes.js
+++ Stage 08/api/src/routes/users.routes.js
@@ -7,5 +7,6 @@ const userRoutes = Router();
 const usersController = new UsersController
 
 userRoutes.post('/', usersController.create)
+userRoutes.put('/:id', usersController.update)
 
 module.exports = userRoutes
