7bceb378a98a976306162e94c640611a30f1577c
guiogigo <guilhermevbatista20@gmail.com>
2024-08-10 19:07:54 -0300

    Stage 08

--- Stage 08/api/src/routes/index.js
+++ Stage 08/api/src/routes/index.js
@@ -1,8 +1,12 @@
 const { Router } = require('express')
 
 const userRoutes = require("./users.routes.js")
+const notesRoutes = require("./notes.routes.js")
+const tagsRouter = require('./tags.routes.js')
 
 const routes = Router()
 routes.use("/users", userRoutes)
+routes.use("/notes", notesRoutes)
+routes.use("/tags", tagsRouter)
 
 module.exports = routes
