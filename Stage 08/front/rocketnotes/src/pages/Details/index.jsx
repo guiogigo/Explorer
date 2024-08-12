1cfc734b897702ea1bec7f579c91c51fd7000da9
guiogigo <guilhermevbatista20@gmail.com>
2024-08-12 18:10:56 -0300

    Started React

---
+++ Stage 08/front/rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,14 @@
+import {Container} from "./styles.js"
+import { Button } from "../../components/Button/index.jsx"
+
+export function Details() {
+  
+  return(
+    <Container>
+      <h1>Hello World!</h1>
+      <Button title="Entrar" loading/>
+      <Button title="Cadastrar" disabled/>
+      <Button title="Voltar"/>
+    </Container>
+  )
+}
